40a1d32614813a5e177291f4c496624c146240f7
kitty <yzhuan02@calpoly.edu>
2021-04-26 15:52:04 -0700

    in the scene 1.1 - Kitty, added a UI element, press the button to save the frame. The picture is saved as a PNG to the screenshot folder. FaceDetector does not apply to plane anymore. Must work with a UI image

--- SecurityAR/Assets/Script/FaceDetector.cs
+++ SecurityAR/Assets/Script/FaceDetector.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using OpenCvSharp;
 
 public class FaceDetector : MonoBehaviour
@@ -8,20 +9,33 @@ public class FaceDetector : MonoBehaviour
     WebCamTexture _webCamTexture;
     CascadeClassifier cascade;
     OpenCvSharp.Rect MyFace;
+    Texture newTexture;
+    public int resWidth = 2550;
+    public int resHeight = 3300;
+
     // Start is called before the first frame update
     void Start()
     {
         WebCamDevice[] devices = WebCamTexture.devices;
+        float width = GetComponentInParent<RectTransform>().rect.width;
+        float height = GetComponentInParent<RectTransform>().rect.height;
+        Debug.Log("width = " + width);
+        Debug.Log("height = " + height);
 
-        _webCamTexture = new WebCamTexture(devices[0].name, 1920, 1080, 60);
+        _webCamTexture = new WebCamTexture(devices[0].name, (int)width, (int)height, 60);
+        //_webCamTexture = new WebCamTexture(devices[0].name);
+
         _webCamTexture.Play();
+        Debug.Log("webcam width = " + _webCamTexture.width);
+        Debug.Log("webcam height = " + _webCamTexture.height);
         cascade = new CascadeClassifier(Application.dataPath + @"/OpenCV+Unity/Demo/Face_Detector/haarcascade_frontalface_default.xml");
     }
 
     // Update is called once per frame
     void Update()
     {
-        GetComponent<Renderer>().material.mainTexture = _webCamTexture;
+        GetComponent<CanvasRenderer>().SetTexture(_webCamTexture);
+        //GetComponent<Renderer>().material.mainTexture = _webCamTexture;
         Mat frame = OpenCvSharp.Unity.TextureToMat(_webCamTexture);
 
         findNewface(frame);
@@ -33,7 +47,7 @@ public class FaceDetector : MonoBehaviour
         var faces = cascade.DetectMultiScale(frame, 1.1, 2, HaarDetectionType.ScaleImage);
         if (faces.Length >= 1)
         {
-            Debug.Log(faces[0].Location);
+            //Debug.Log(faces[0].Location);
             MyFace = faces[0];
         }
     }
@@ -44,7 +58,27 @@ public class FaceDetector : MonoBehaviour
         {
             frame.Rectangle(MyFace, new Scalar(250, 0, 0), 2);
         }
-        Texture newTexture = OpenCvSharp.Unity.MatToTexture(frame);
-        GetComponent<Renderer>().material.mainTexture = newTexture;
+        newTexture = OpenCvSharp.Unity.MatToTexture(frame);
+        
+        //GetComponent<Renderer>().material.mainTexture = newTexture;
+        GetComponent<CanvasRenderer>().SetTexture(newTexture);
     }
+
+    public static string ScreenShotName(int width, int height)
+    {
+        return string.Format("{0}/screenshots/screen_{1}x{2}_{3}.png",
+                             Application.dataPath,
+                             width, height,
+                             System.DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss"));
+    }
+
+    public void takePhoto()
+    {
+        Texture2D screenShot = (Texture2D)newTexture;
+        byte[] bytes = screenShot.EncodeToPNG();
+        string filename = ScreenShotName(resWidth, resHeight);
+        System.IO.File.WriteAllBytes(filename, bytes);
+        Debug.Log(string.Format("Took screenshot to: {0}", filename));
+    }
+
 }

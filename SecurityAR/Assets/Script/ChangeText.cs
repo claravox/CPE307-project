c60c85f93780e33b6c8a815e38d08f591ef62857
kitty <yzhuan02@calpoly.edu>
2021-06-02 02:59:08 -0700

    improve UI significantly, image blur works, photo capture works

---
+++ SecurityAR/Assets/Script/ChangeText.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ChangeText : MonoBehaviour
+{
+    TMP_Text liveText;
+    bool status;
+    private void Start()
+    {
+        liveText = this.GetComponent<TMP_Text>();
+        liveText.text = "ON";
+        status = true;  
+    }
+    public void changeTextForLiveMode()
+    {
+        status = !status;
+        if (status) liveText.text = "ON";
+        else liveText.text = "OFF";
+    }
+}
---
+++ SecurityAR/Assets/Script/ImageOverlayManager.cs
@@ -0,0 +1,106 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Rect = OpenCVForUnity.CoreModule.Rect;
+using OpenCVForUnity.CoreModule;
+
+public class ImageOverlayManager : MonoBehaviour
+{
+    public GameObject ImagePrefab;
+ 
+    Rect[] faces;
+    static int lastFaceCount = 0;
+    List<GameObject> images = new List<GameObject>();
+    Transform LastRemovedImgTransform;
+    string Imagetype;
+    void Start()
+    {
+        Imagetype = "flower";
+        this.enabled = false;
+    }
+
+    void FixedUpdate()
+    {
+        //get face count
+        faces = new Rect[0];
+        //if face count changed, change the amount of images as well
+        //there is always 1 image less the faces
+        if (lastFaceCount != faces.Length)
+        {
+            
+            int change = lastFaceCount - faces.Length;
+            //less face, destroy image that's newly added
+            if (change > 0)
+            {
+                for (int i = 0; i < change; i++)
+                {
+                    
+                    LastRemovedImgTransform = images[images.Count - 1].transform;
+                    GameObject.Destroy(images[images.Count - 1]);
+                    images.RemoveAt(images.Count-1);
+                }
+            }
+            //more face, add image
+            else if (change < 0)
+            {
+                change = -change;
+                for (int i = 0; i < change; i++)
+                {
+                    instantiateImage();
+                }
+            }
+            lastFaceCount = faces.Length;
+        }
+
+        // each image follows a face in the array except the first face
+        for (int i = 0; i < lastFaceCount; i++)
+        {
+            Rect face = faces[i];
+            images[i].GetComponent<ImageFollowFace>().faceLocation = face;
+
+            if (i == 0)
+            {
+                images[i].GetComponent<Image>().enabled = false;
+            }
+        }
+    }
+
+    void instantiateImage()
+    {
+        Transform Spawn = this.gameObject.transform;
+        if(LastRemovedImgTransform != null)
+        {
+            Spawn = LastRemovedImgTransform;
+        }
+        GameObject aImage = GameObject.Instantiate(ImagePrefab, Spawn);
+        images.Add(aImage);
+    }
+
+    public void changeImageType(string name)
+    {
+        Imagetype = name;
+        if (images.Count == 0)
+            return;
+        foreach(GameObject img in images)
+        {
+            img.GetComponent<ImageFollowFace>().changeImage(name);
+        }
+    }
+
+    public void enableImage()
+    {
+        this.enabled = true;
+    }
+
+    public void disableImage()
+    {
+        foreach(GameObject img in images)
+        {
+            GameObject.Destroy(img);
+        }
+        images.Clear();
+        lastFaceCount = 0;
+        this.enabled = false;
+    }
+}
